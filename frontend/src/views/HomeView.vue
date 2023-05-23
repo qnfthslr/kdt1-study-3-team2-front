11288777e9f2592f02ceba80a3493b1995b5a3af
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-24 00:58:29 +0900

    feat: [이상원] 버튼 클릭시 회원가입 UI 생성 [VPP-ACCOUNT-REGIST-1]

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,26 @@
 <template>
-  <hello-world />
+  <v-container>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/product-account-register-page">회원 가입</router-link>
+
+
+    </nav>
+
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
+import HelloWorld from '../components/HelloWorld'
 
-    components: {
-      HelloWorld,
-    },
-  }
+
+export default {
+  name: 'Home',
+
+  components: {
+
+  },
+}
 </script>
---
+++ frontend/src/views/product/ProductAccountRegisterPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-btn color="red" @click="readyToCreateAccount">회원 가입 준비 버튼</v-btn>
+        <div v-if="isPressedButton">
+            <product-register-form align="center" @submit="onSubmit" />
+        </div>
+
+
+
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        readyToCreateAccount() {
+            this.isPressedButton = true
+            alert('제품 등록 준비')
+        },
+    }
+}
+</script>
+<style lang="">
+    
+</style>
