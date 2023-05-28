168f57af5b4c0ae137d011da72874947be0ecd3d
iknowca <iknowca@gmail.com>
2023-05-29 02:22:26 +0900

    feat: [김원기] 로그인 기능 구현 [VPP-ACCOUNT-LOGIN-1]

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,9 +3,10 @@
     <div>
       <nav>
 
-        <router-link to="/product-register-page">상품 등록</router-link>
+        <router-link to="/product-register-page">상품 등록</router-link>|
         <router-link to="/account-register-page">회원 가입</router-link> |
         <router-link to="/account-list-page">회원 목록</router-link> |
+        <router-link to="/account-login-page">로그인</router-link>|
       </nav>
     </div>
   </v-container>
