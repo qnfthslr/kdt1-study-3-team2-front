397d8ff881d62295d87763d5f3a02aba9273685a
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-28 20:45:00 +0900

    refactor: [이상원] 파일 이름 수정2

---
+++ frontend/src/router/account/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import AccountRegisterPage from "@/views/account/AccountRegisterPage.vue"
+import AccountListPage from "@/views/account/AccountListPage.vue"
+
+Vue.use(VueRouter);
+
+const accountRoutes = [
+
+    {
+        path: "account-register-page",
+        name: "AccountRegisterPage",
+        component: AccountRegisterPage
+    },
+    {
+        path: "account-list-page",
+        name: "AccountListPage",
+        component: AccountListPage
+    }
+
+];
+export default accountRoutes;
