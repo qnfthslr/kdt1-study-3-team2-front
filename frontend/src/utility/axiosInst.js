f34948bc5ef4d54772433742eb27edb87875804e
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-05-28 19:02:30 +0900

    feat: [이상원] AWS 배포 설정 및 accountList 파일 생성

--- frontend/src/utility/axiosInst.js
+++ frontend/src/utility/axiosInst.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import env from "@/env";
 
 // Legacy 구성은 아래를 참고
 const axiosInst = axios.create({
-  baseURL: "http://localhost:7777",
+  baseURL: env.api.MAIN_API_URL,
   //baseURL: 'http://192.168.20.3:7777',
   timeout: 2500,
 });
---
+++ frontend/src/views/product/ProductAccountListPage.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div>
+        
+        <h2>상품 목록</h2>
+
+        <product-account-list-form :accounts="accounts"/>
+
+
+
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
