12dffe8a37a28ad96337f7f787be8e3dfa33b8e7
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-06-02 01:11:47 +0900

    update: [이상원] 상품 정보 보기 기능 추가 (사진 제외)

---
+++ frontend/src/components/product/ProductReadForm.vue
@@ -0,0 +1,49 @@
+<template lang="">
+    
+        <v-simple-table>
+            <template v-slot:default>
+                <tbody>
+                    <tr>
+                        <th>상품명</th>
+                        <td>{{ product.productName }}</td>
+                    </tr>
+                    <tr>
+                        <th>가격</th>
+                        <td>{{ product.productPrice }}</td>
+                    </tr>
+                    
+                    <tr>
+                        <th>세부 정보</th>
+                        <td>{{ product.productDetails }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>등록한 계정</th>
+                        <td>{{ product.account }}</td>
+                    </tr>
+
+                </tbody>    
+            
+                
+            </template>
+
+
+        </v-simple-table>
+
+    
+</template>
+<script>
+export default {
+    name: "ProductReadForm",
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        }
+    }
+
+}
+</script>
+<style lang="">
+    
+</style>
